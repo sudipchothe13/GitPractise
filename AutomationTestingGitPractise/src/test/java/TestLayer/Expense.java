cba936873f925bdeed1c37d62602bbb9f0957149
Sudip Chothe <sudipchothe13@gmail.com>
2026-06-29 22:22:01 +0530

    Expense first commit

---
+++ AutomationTestingGitPractise/src/test/java/TestLayer/Expense.java
@@ -0,0 +1,13 @@
+package TestLayer;
+
+public class Expense {
+
+	public static void main(String[] args) {
+		addExpense();
+	}
+	
+	public static void addExpense() {
+	    System.out.println("Expense Added");
+	}
+
+}
