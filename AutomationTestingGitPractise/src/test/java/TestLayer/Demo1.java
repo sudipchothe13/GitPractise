5d179c93488ba03d2549e69b1ed014640c1d0c3d
Sudip Chothe <sudipchothe13@gmail.com>
2026-07-04 21:33:56 +0530

    Jenkins

--- AutomationTestingGitPractise/src/test/java/TestLayer/Demo1.java
+++ AutomationTestingGitPractise/src/test/java/TestLayer/Demo1.java
@@ -1,8 +1,15 @@
 package TestLayer;
 
+import org.testng.annotations.Test;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
+		System.out.println("Git practise");
+	}
+	
+	@Test
+	public void demo() {
 		System.out.println("Git practise");
 	}
 }
