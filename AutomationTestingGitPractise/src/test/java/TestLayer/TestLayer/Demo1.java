b85785ca3a4845b684a123f16806474a4f264480
Sudip Chothe <sudipchothe13@gmail.com>
2026-07-04 22:40:34 +0530

    t

--- AutomationTestingGitPractise/src/test/java/TestLayer/TestLayer/Demo1.java
+++ AutomationTestingGitPractise/src/test/java/TestLayer/TestLayer/Demo1.java
@@ -4,10 +4,7 @@ import org.testng.annotations.Test;
 
 public class Demo1 {
 
-	public static void main(String[] args) {
-		System.out.println("Git practise with Sudip");
-		System.out.println("Git practise with Sudip Chothe");
-	}
+	
 	
 	@Test (groups = {"Regression", "SendRequest"})
 	public void demo() {
